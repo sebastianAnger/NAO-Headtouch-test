e2518e1225b1393aa1a3a1dc49f1c6ad8e559d41
sebastianAnger <sebastiananger.95@gmail.com>
2025-02-12 12:15:27 -0700

    Add files via upload

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,295 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Boton01(Parado)/CONVER/Animated Say</name>
+        <message>
+            <source>Hola alondra, me siento muy feliz y agradecido de estar aqui</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hola alondra, me siento muy feliz y agradecido de estar aqui</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton01(Parado)/CONVER/Animated Say (1)</name>
+        <message>
+            <source>Quiero ser un robot famoso y ayudar a los niños con autismo de crit sonora</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Quiero ser un robot famoso y ayudar a los niños con autismo de crit sonora</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton01(Parado)/CONVER/Say</name>
+        <message>
+            <source>muchas gracias a ustedes, vengan a visitarme, gracias crit sonora</source>
+            <comment>Text</comment>
+            <translation type="obsolete">muchas gracias a ustedes, vengan a visitarme, gracias crit sonora</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton01(Parado)/CONVER/Say (1)</name>
+        <message>
+            <source>me gustan los burros percherones</source>
+            <comment>Text</comment>
+            <translation type="obsolete">me gustan los burros percherones</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton01(Parado)/HOLA</name>
+        <message>
+            <source>HOLA a todos, un gusto conocerlos.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">HOLA a todos, un gusto conocerlos.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton01(Parado)/Hola 2</name>
+        <message>
+            <source>soy NAO un gusto conocerlos a todos</source>
+            <comment>Text</comment>
+            <translation type="obsolete">soy NAO un gusto conocerlos a todos</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton01(Parado)/Say</name>
+        <message>
+            <source>HOLA a todos, un gusto conocerlos. Bienvenidos a crit sonora.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">HOLA a todos, un gusto conocerlos. Bienvenidos a crit sonora.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton01(Parado)/Say (1)</name>
+        <message>
+            <source>soy NAO un gusto conocerlos a todos</source>
+            <comment>Text</comment>
+            <translation type="obsolete">soy NAO un gusto conocerlos a todos</translation>
+        </message>
+        <message>
+            <source>soy NAO un gusto conocerlos a todos blblablablablablablablablabla</source>
+            <comment>Text</comment>
+            <translation type="obsolete">soy NAO un gusto conocerlos a todos blblablablablablablablablabla</translation>
+        </message>
+        <message>
+            <source>soy NAO un gusto conocerlos a todos bla bla bla bla bla bla bla bla bla bla</source>
+            <comment>Text</comment>
+            <translation type="obsolete">soy NAO un gusto conocerlos a todos bla bla bla bla bla bla bla bla bla bla</translation>
+        </message>
+        <message>
+            <source>soy NAO un gusto conocerlos a todos bla bla</source>
+            <comment>Text</comment>
+            <translation type="obsolete">soy NAO un gusto conocerlos a todos bla bla</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton02(Medio)/Animated Dialogo1</name>
+        <message>
+            <source>Hola, dejame decirte que es lo que hago</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hola, dejame decirte que es lo que hago</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton02(Medio)/Animated Say</name>
+        <message>
+            <source>Alejandro, dejame decirte que es lo que hago</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Alejandro, dejame decirte que es lo que hago</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton02(Medio)/Animated Say (1)</name>
+        <message>
+            <source>Quiero ser un robot famoso y ayudar a los niños con autismo de crit sonora</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Quiero ser un robot famoso y ayudar a los niños con autismo de crit sonora</translation>
+        </message>
+        <message>
+            <source>Quiero ser un robot famoso</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Quiero ser un robot famoso</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton02(Medio)/Dialogo 2</name>
+        <message>
+            <source>hago muchas cosas</source>
+            <comment>Text</comment>
+            <translation type="obsolete">hago muchas cosas</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton02(Medio)/DialogoFinal</name>
+        <message>
+            <source>Vengo dispuesto a trabajar, me gusta la musica y conocer gente.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Vengo dispuesto a trabajar, me gusta la musica y conocer gente.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton02(Medio)/Say</name>
+        <message>
+            <source>El autismo es muy importante el tema de autismo por que hay muchos casos y en ucos niños y muchas cosas mucho texto</source>
+            <comment>Text</comment>
+            <translation type="obsolete">El autismo es muy importante el tema de autismo por que hay muchos casos y en ucos niños y muchas cosas mucho texto</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton02(Medio)/Say (2)</name>
+        <message>
+            <source>Vengo dispuesto a trabajar, me gustan los burros de asada, el gym y ver a los naranjeros de hermosillo. Me gusta la COCACOLA</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Vengo dispuesto a trabajar, me gustan los burros de asada, el gym y ver a los naranjeros de hermosillo. Me gusta la COCACOLA</translation>
+        </message>
+        <message>
+            <source>Vengo dispuesto a trabajar, me gustan los burros de asada, el gimnasio y ver a los naranjeros de hermosillo. Me gusta la COCACOLA</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Vengo dispuesto a trabajar, me gustan los burros de asada, el gimnasio y ver a los naranjeros de hermosillo. Me gusta la COCACOLA</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton03(atras)/Adios</name>
+        <message>
+            <source>Muchas gracias, me despido me despido, esto es una despedida</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Muchas gracias, me despido me despido, esto es una despedida</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton03(atras)/BAILE/Say</name>
+        <message>
+            <source>Muchas gracias, me despido me despido, esto es una despedida</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Muchas gracias, me despido me despido, esto es una despedida</translation>
+        </message>
+        <message>
+            <source>ADIOOOOOOOOOS</source>
+            <comment>Text</comment>
+            <translation type="obsolete">ADIOOOOOOOOOS</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton03(atras)/Say</name>
+        <message>
+            <source>Muchas gracias, me despido me despido, esto es una despedida</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Muchas gracias, me despido me despido, esto es una despedida</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Boton03(atras)/Say (1)</name>
+        <message>
+            <source>PERO ANTES D EIRME VAMOS A BAILAAAAAAr</source>
+            <comment>Text</comment>
+            <translation type="obsolete">PERO ANTES D EIRME VAMOS A BAILAAAAAAr</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/DialogoSitDown/Say</name>
+        <message>
+            <source>HOLA</source>
+            <comment>Text</comment>
+            <translation type="obsolete">HOLA</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/DialogoSitDown/Say (1)</name>
+        <message>
+            <source>Me gusta el mazapan</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Me gusta el mazapan</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/DialogoSitDown/Say (2)</name>
+        <message>
+            <source>A que horas juegan los naranjeros</source>
+            <comment>Text</comment>
+            <translation type="obsolete">A que horas juegan los naranjeros</translation>
+        </message>
+        <message>
+            <source>A que horas juegan todos</source>
+            <comment>Text</comment>
+            <translation type="obsolete">A que horas juegan todos</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/FrontTouch(DePie/StandUp)/HOLA</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>HOLA a todos, un gusto conocerlos.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">HOLA a todos, un gusto conocerlos.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/FrontTouch(DePie/StandUp)/Hola 2</name>
+        <message>
+            <source>soy NAO un gusto conocerlos a todos</source>
+            <comment>Text</comment>
+            <translation type="obsolete">soy NAO un gusto conocerlos a todos</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/FrontTouch(DePie/StandUp)/Present</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>soy NAO un gusto conocerlos a todos</source>
+            <comment>Text</comment>
+            <translation type="unfinished">soy NAO un gusto conocerlos a todos</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/middleTouched(Conversation)/Animated Dialogo1</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hola, dejame decirte que es lo que hago</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hola, dejame decirte que es lo que hago</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/middleTouched(Conversation)/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Quiero ser un robot famoso</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Quiero ser un robot famoso</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/middleTouched(Conversation)/Dialogo 2</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>hago muchas cosas</source>
+            <comment>Text</comment>
+            <translation type="unfinished">hago muchas cosas</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/middleTouched(Conversation)/DialogoFinal</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Vengo dispuesto a trabajar, me gusta la musica y conocer gente.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Vengo dispuesto a trabajar, me gusta la musica y conocer gente.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/rearTouched(bye)/Adios</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Muchas gracias, me despido me despido, esto es una despedida</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Muchas gracias, me despido me despido, esto es una despedida</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/rearTouched(bye)/BAILE/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>ADIOOOOOOOOOS</source>
+            <comment>Text</comment>
+            <translation type="unfinished">ADIOOOOOOOOOS</translation>
+        </message>
+    </context>
+</TS>
---
+++ translations/translation_es_ES.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+    <context>
+        <name>behavior_1/behavior.xar:/FrontTouch(DePie/StandUp)/HOLA</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>HOLA a todos, un gusto conocerlos.</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/FrontTouch(DePie/StandUp)/Present</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>soy NAO un gusto conocerlos a todos</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/middleTouched(Conversation)/Animated Dialogo1</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hola, dejame decirte que es lo que hago</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/middleTouched(Conversation)/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Quiero ser un robot famoso</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/middleTouched(Conversation)/Dialogo 2</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>hago muchas cosas</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/middleTouched(Conversation)/DialogoFinal</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Vengo dispuesto a trabajar, me gusta la musica y conocer gente.</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/rearTouched(bye)/Adios</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Muchas gracias, me despido me despido, esto es una despedida</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/rearTouched(bye)/BAILE/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>ADIOOOOOOOOOS</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
